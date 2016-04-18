d59d9f5d330b5d1dbc28f946e5d87a17e9fb3490
Unknow0 <unknow0@free.fr>
2016-04-18 16:37:47 +0200

    fix first day of month

--- lalcal.c
+++ lalcal.c
@@ -260,14 +260,15 @@ void paintCalendar(Display * display, GC gc, Window calendar, XftDraw * caldraw,
 	{
 	int i = 0;
 	int p_x = 0, p_y = 0;
+	int fdw;
 	int eom=end_of_month(&draw_cal);
 
 	if(draw_cal.tm_year==curtime->tm_year&&draw_cal.tm_mon==curtime->tm_mon)
 		{
 		p_x=(curtime->tm_wday-startday)*glyph_w*3-1;
 		i=(curtime->tm_mday+(draw_cal.tm_wday-startday))/7;
-	    p_y=(2+i)*glyph_h+5;
-	    XftDrawRect(caldraw, xfthlcolor, p_x, p_y, glyph_w*2+6, glyph_h+4);
+	    p_y=(2+i)*glyph_h+8;
+	    XftDrawRect(caldraw, xfthlcolor, p_x, p_y, glyph_w*2+6, glyph_h);
 		}
 
 	strftime(draw_line, 50, "%B %Y", &draw_cal);
@@ -294,7 +295,10 @@ void paintCalendar(Display * display, GC gc, Window calendar, XftDraw * caldraw,
 	while(i!=startday);
 
 	i=1;
-	p_x=2+(draw_cal.tm_wday-startday)*glyph_w*3;
+	fdw=draw_cal.tm_wday-startday;
+	if(fdw<0)
+		fdw+=7;
+	p_x=2+fdw*glyph_w*3;
 	p_y+=glyph_h+2;
 	while(i<=eom)
 		{
